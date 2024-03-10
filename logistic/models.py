be7100f1f3ad2d47bbb3381d14236e7d5e0f93e0
Ing-Daron11 <daronmercadog@gmail.com>
2024-03-10 16:42:48 -0500

    feat: create model and migration of event

--- logistic/models.py
+++ logistic/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-
+# In here, we create the clases that later on, they turn into sql tables
 # Create your models here.
 
 class Event(models.Model):
     registerDate = models.DateField()
+    name = models.CharField(max_length = 200)
+    executionDate = models.DateField()
+    place = models.CharField(max_length = 200)
+    manager = models.CharField(max_length = 200)
+    progress = models.IntegerField() #en revision
+    finishDate = models.DateField()
+    #bitacora = models.#####() #en revision
--- logistic/views.py
+++ logistic/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+# In here, we must create the HTMLs
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Home page")
+
