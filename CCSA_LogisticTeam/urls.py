cb1ba8d666a0198fe61939b549fb12480d47bf8d
CamiloBueno <camilobueno05@gmail.com>
2024-03-11 16:53:55 -0500

    fix: set urls and imports

--- CCSA_LogisticTeam/urls.py
+++ CCSA_LogisticTeam/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from logistic import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup'),
-    path('logout/', views.signout, name='logout'),
-    path('signin/', views.signin, name='signin')
+    path('', include('logistic.urls'))
 ]
--- logistic/views.py
+++ logistic/views.py
@@ -1,12 +1,18 @@
 from django.db import IntegrityError
 from django.contrib.auth.models import User
-from django.shortcuts import redirect, render
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
+
 
 def signup(request):
     if request.method == 'GET':
@@ -16,7 +22,8 @@ def signup(request):
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
                 login(request, user)
                 return redirect('home')
@@ -30,9 +37,11 @@ def signup(request):
             'error': 'Password do not match',
         })
 
+
 def signout(request):
     logout(request)
     return redirect('home')
+
 
 def signin(request):
     if request.method == 'GET':
@@ -40,7 +49,8 @@ def signin(request):
             'form': AuthenticationForm
         })
     else:
-        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
             return render(request, 'signin.html', {
                 'form': AuthenticationForm,
