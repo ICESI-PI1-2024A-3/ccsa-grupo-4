757cb16609cab60a1efd87c70c4de5a3fe5d0ef9
Santiago Arboleda Velasco <santiagoarboledavelasco@gmail.com>
2024-03-17 17:25:27 -0500

    feat: update folders

--- logistic/views.py
+++ logistic/views.py
@@ -10,7 +10,7 @@ def user_detail(request):
         users = User.objects.filter(name__icontains=query) | User.objects.filter(
             id_number__icontains=query)
 
-        return render(request, 'users.html', {'users': users})
+        return render(request, 'users/users.html', {'users': users})
 
 
 def search_users(request):
@@ -28,4 +28,4 @@ def search_users(request):
 
         users = User.objects.filter(id_number=id_query)
 
-    return render(request, 'users_search.html', {'users': users})
+    return render(request, 'users/users_search.html', {'users': users})
