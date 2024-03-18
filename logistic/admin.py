d098bffe0ec7e97034ee389b23f8fa0aebe5d46b
Santiago Arboleda Velasco <santiagoarboledavelasco@gmail.com>
2024-03-18 02:24:22 -0500

    feat: implement the hu template

--- logistic/admin.py
+++ logistic/admin.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 from .models import User
 
 
-class UserAdmin(admin.ModelAdmin):
-    exclude = ('department',)
-
-
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
