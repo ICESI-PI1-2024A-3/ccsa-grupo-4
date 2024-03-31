e554ef55687368e74b21b579d090d90c96af3fdd
Johan Diaz <joansebas9021@hotmail.com>
2024-03-31 17:49:19 -0500

    test: add test for event checklist

--- logistic/test/forms_test.py
+++ logistic/test/forms_test.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.forms import DateTimeInput
 import logistic.forms
 from logistic.forms.eventForm import EventForm
 from logistic.forms.taskForm import TaskForm
@@ -32,8 +33,40 @@ class forms_test(TestCase):
     def test_field_labels(self):
         event_form = EventForm()
         task_form = TaskForm()
-        task_checklist_form = TaskChecklist()  # Utiliza TaskChecklist importado correctamente
+        task_checklist_form = TaskChecklist()  
+
 
         self.assertEqual(event_form.fields['name'].label, 'Nombre')
+        self.assertEqual(event_form.fields['executionDate'].label, 'Fecha de Ejecución')
+        self.assertEqual(event_form.fields['place'].label, 'Lugar')
+        self.assertEqual(event_form.fields['progress'].label, 'Progreso')
+        self.assertEqual(event_form.fields['finishDate'].label, 'Fecha de Finalización')
+        self.assertEqual(event_form.fields['important'].label, 'Importante')
+        self.assertEqual(event_form.fields['user'].label, 'Usuario')
+        
+        self.assertEqual(task_form.fields['name'].label, 'Nombre')
         self.assertEqual(task_form.fields['event'].label, 'Evento')
+        
         self.assertEqual(task_checklist_form.fields['done'].label, 'Hecho')
+        
+    def test_required_fields(self):
+        event_form = EventForm(data={})
+        task_form = TaskForm(data={})
+        
+        self.assertFalse(event_form.is_valid())
+        self.assertIn('name', event_form.errors)
+        self.assertIn('executionDate', event_form.errors)
+        self.assertIn('place', event_form.errors)
+        self.assertIn('finishDate', event_form.errors)
+        self.assertIn('user', event_form.errors)
+        
+        self.assertFalse(task_form.is_valid())
+        self.assertIn('name', task_form.errors)
+        self.assertIn('event', task_form.errors)
+        
+    def test_date_time_format(self):
+        event_form = EventForm()
+        
+        self.assertIsInstance(event_form.fields['executionDate'].widget, DateTimeInput)
+        self.assertIsInstance(event_form.fields['finishDate'].widget, DateTimeInput)
+        
