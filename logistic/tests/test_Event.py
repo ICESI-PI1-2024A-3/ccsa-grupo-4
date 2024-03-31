6b144ee4175c1431249625f27673dcdac67beb1f
Ing-Daron11 <daronmercadog@gmail.com>
2024-03-31 13:26:08 -0500

    test: add test for creating event #2 and docstring

--- logistic/tests/test_Event.py
+++ logistic/tests/test_Event.py
@@ -15,8 +15,25 @@ class test_Event(TestCase):
         self.user = User.objects.create_user(username='testuser', password='12345')
         self.event = Event.objects.create(name='Test Event', executionDate=timezone.now() + timedelta(days=7), place='Test Place', progress=0, user=self.user)
 
-    #Probamos que se puede crear un nuevo evento
+
     def test_create_event(self):
+        """ Test for: creating a new event in the database.
+        
+        - First, we authenticate the user with force_login.
+        - Then, we capture the response of the page when creating the event.
+        - Finally, after creating the event, the status code of the page would be 200, which is 'OK' and means that the request was successful.
+        """
         self.client.force_login(self.user)
         response = self.client.post(reverse('create_event'), {'name': 'New Event', 'executionDate': timezone.now() + timedelta(days=7), 'place': 'New Place', 'progress': 0})
-        self.assertEqual(response.status_code, 200)  # el código 200 significa 'OK' y que se agregó 
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_create_event2(self):
+        """ Test for: Creating a new event with a not valid input
+
+        - The response of this would be 200, since the request was succesfull
+          and it doesn't show up any unexpected error, just the normal ValueError that is controled
+        """
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('create_event'), {'name': 'New Event2', 'executionDate': "valorNoValido", 'place': 'Hall de auditorios', "progress": 20 })
+        self.assertEqual(response.status_code, 200)
