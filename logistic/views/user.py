a193f87a12c15713cc95929e1ede5c419809b063
CamiloBueno <camilobueno05@gmail.com>
2024-03-31 18:57:27 -0500

    feat:add delete in user profile

--- logistic/views/user.py
+++ logistic/views/user.py
@@ -1,5 +1,4 @@
 from django.db import IntegrityError
-from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
