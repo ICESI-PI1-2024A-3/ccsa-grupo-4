b8a3dac51761ba2a9920673434381ca0f6536eb9
Ing-Daron11 <daronmercadog@gmail.com>
2024-04-01 13:45:18 -0500

    fix: fix small error

--- logistic/urls.py
+++ logistic/urls.py
@@ -9,7 +9,6 @@ from django.contrib.auth.views import PasswordResetCompleteView
 from django.contrib.auth.views import PasswordResetDoneView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('home/', user.home, name='home'),
     path('signup/', user.signup, name='signup'),
     path('logout/', user.signout, name='logout'),
