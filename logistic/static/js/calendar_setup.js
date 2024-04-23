1ffa08572a2c178c2a1a2c44c400ae6331e5575c
Cris2002-CAR <84110042+Cris2002-CAR@users.noreply.github.com>
2024-04-23 15:21:34 -0500

    fix: colors in calendar

--- logistic/static/js/calendar_setup.js
+++ logistic/static/js/calendar_setup.js
@@ -8,11 +8,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // Creamos un elemento para el título del evento
             var titleElement = document.createElement('div');
+            titleElement.style.color = arg.event.backgroundColor; // Usa el color de fondo del evento
+            titleElement.style.fontWeight = 'bold'; // Aplica negrita
             titleElement.innerText = arg.event.title;
+            
 
             // Creamos un elemento para el nombre del usuario
             var userElement = document.createElement('div');
+            userElement.style.color = arg.event.backgroundColor; // Usa el color de fondo del evento
+            userElement.style.fontWeight = 'bold'; // Aplica negrita
             userElement.innerText = "Usuario: " + arg.event.extendedProps.username;
+
 
             // Creamos un contenedor para los elementos
             var arrayOfDomNodes = [titleElement, userElement]
