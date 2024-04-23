d8e75bc39d5e5420ca8f452e87f2c2a6269b5415
Ing-Daron11 <daronmercadog@gmail.com>
2024-04-23 16:17:39 -0500

    fix: add more settings to deployment

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
-# poetry install
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
