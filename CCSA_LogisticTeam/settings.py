6225abae77b979c16dd1344090f28ae4add57a6f
Ing-Daron11 <daronmercadog@gmail.com>
2024-04-23 16:32:43 -0500

    fix: fix typing error

--- CCSA_LogisticTeam/settings.py
+++ CCSA_LogisticTeam/settings.py
@@ -146,7 +146,7 @@ STATIC_URL = '/static/'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressdManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
