5b5b8f17ee774f6e2d437195b29dba669dbe5fbb
Johan Diaz <joansebas9021@hotmail.com>
2024-05-06 13:43:57 -0500

    feature-alerts: cambio de correo

--- CCSA_LogisticTeam/settings.py
+++ CCSA_LogisticTeam/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sebasx266@gmail.com'
-EMAIL_HOST_PASSWORD = 'mutu uhez aahj hmmh'
+EMAIL_HOST_USER = 'apoyologisticoccsa@gmail.com'
+EMAIL_HOST_PASSWORD = 'cfrqhiskdcuxeill'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # settings.py
 
