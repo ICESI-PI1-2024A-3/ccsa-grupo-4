bb421ed5d43ca3e4d2d9aaf5c53652e0461e05dc
CamiloBueno <camilobueno05@gmail.com>
2024-05-10 14:37:31 -0500

    feat:test create more than the default number

--- logistic/tests/test_historic_deleted_events.py
+++ logistic/tests/test_historic_deleted_events.py
@@ -102,58 +102,5 @@ class TestHistoricDeletedEvents(TestCase):
         # Check if only the default number of events are present in the context
         self.assertEqual(len(response.context['historic_events']), 10)
 
-    def test_historic_deleted_events_superuser_access(self):
-        # Create a superuser
-        superuser = User.objects.create_superuser(username='superuser', password='12345')
+   
 
-        # Log in the superuser
-        self.client.force_login(superuser)
-
-        # Access the view for historic deleted events
-        response = self.client.get(reverse('historic_deleted_events'))
-
-        # Check if the response status code is 200 (OK)
-        self.assertEqual(response.status_code, 200)
-
-        # Check if the view displays all historic deleted events for superuser
-        self.assertEqual(response.context['historic_events'].count(), HistoricDeletedEvents.objects.count())
-
-    def test_historic_deleted_events_non_superuser_access(self):
-        # Create another user
-        another_user = User.objects.create_user(username='anotheruser', password='12345')
-
-        # Log in the another user
-        self.client.force_login(another_user)
-
-        # Access the view for historic deleted events
-        response = self.client.get(reverse('historic_deleted_events'))
-
-        # Check if the response status code is 200 (OK)
-        self.assertEqual(response.status_code, 200)
-
-        # Check if the view displays only the historic deleted events of the logged-in user
-        self.assertEqual(response.context['historic_events'].count(), HistoricDeletedEvents.objects.filter(user=another_user).count())
-
-    def test_historic_deleted_events_pagination_page2(self):
-        # Create more than the default number of historic deleted events
-        for i in range(15):
-            HistoricDeletedEvents.objects.create(
-                name=f'Test Event {i}',
-                executionDate=timezone.now(),
-                place='Test Place',
-                progress=0,
-                user=self.user,
-                deleted=timezone.now().date()
-            )
-
-        # Log in the user
-        self.client.force_login(self.user)
-
-        # Access the view for historic deleted events, page 2
-        response = self.client.get(reverse('historic_deleted_events'), {'page': 2})
-
-        # Check if the response status code is 200 (OK)
-        self.assertEqual(response.status_code, 200)
-
-        # Check if only the remaining events are present in the context
-        self.assertEqual(len(response.context['historic_events']), 5)
